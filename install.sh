f93fde45af7a1661f95aa436613d3dcd843d2c08
Alexey Zavistovich <80489200+dancon-playrix@users.noreply.github.com>
2024-05-09 13:32:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ sudo rm -rf $HOME/homebrew/plugins/SDH-PauseGames
 
 echo "installing SDH-PauseGames plugin for TDP control"
 # download + installhttps://github.com/aarron-lee/SDH-PauseGames/tree/main
-curl -L $(curl -s https://api.github.com/repos/dancon-playrix/SDH-PauseGames/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o "$HOME/SDH-PauseGames.tar.gz"
-sudo tar -xzf "SDH-PauseGames.tar.gz" -C $HOME/homebrew/plugins
+curl -L $(curl -s https://api.github.com/repos/dancon-playrix/SDH-PauseGames/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o "$HOME/SDH-PauseGames.zip"
+sudo unzip "SDH-PauseGames.zip" -C $HOME/homebrew/plugins
 
 # install complete, remove build dir
 rm  "$HOME/SDH-PauseGames.tar.gz"
